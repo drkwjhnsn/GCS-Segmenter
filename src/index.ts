ce7a78a4495f853c0bd737512d0913f1b8ff0cd7
Derek Johnson <derek.johnson@appstem.com>
2020-10-02 14:34:12 -0700

    ...

--- src/index.ts
+++ src/index.ts
@@ -139,33 +139,6 @@ const processVideo = async (bucketName: string, gcsFilePath: string, email: stri
     const baseUrl = `${CDN_HOST}/${OPEN_BUCKET}/${title}/`;
     fs.writeFileSync(path.join(tmpDir, `${title}.keyinfo`), keyInfo);
     console.log(originalFilePath)
-    // execSync(
-    //   `${ffmpeg_static}  -y \
-    //   -i ${originalFilePath} \
-    //   -c:a copy \
-    //   -hls_enc_key key.info \
-    //   -preset fast -sc_threshold 0 \
-    //   -c:v libx264 \
-    //   -filter:v fps=30 -g 60 \
-    //   -map 0 -s:v:0 426x240 -b:v:1 192k \
-    //   -map 0 -s:v:1 640x360 \
-    //   -map 0 -s:v:2 854x480 \
-    //   -map 0 -s:v:3 1280x720 \
-    //   -map 0 -s:v:4 1920x1080 \
-    //   -map 0 -s:v:5 2560x1440 \
-    //   -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" \
-    //   -f hls \
-    //   -hls_base_url "${baseUrl}" \
-    //   -hls_enc 1 \
-    //   -hls_key_info_file "${title}.keyinfo" \
-    //   -master_pl_name master.m3u8 \
-    //   -hls_time 6 \
-    //   -hls_list_size 0 \
-    //   -hls_playlist_type vod \
-    //   -hls_segment_filename "${tmpDir}/v%vfileSequence%d.ts" \
-    //   ${tmpDir}/v%vprog_index.m3u8`,
-    //   { cwd: tmpDir }
-    // );
     execSync(
       `${ffmpeg_static}  -y \
       -i ${originalFilePath} \
@@ -182,15 +155,45 @@ const processVideo = async (bucketName: string, gcsFilePath: string, email: stri
       -map 0 -s:v:5 2560x1440 \
       -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" \
       -f hls \
-      -hls_key_info_file "${title}.keyinfo" \
+      -hls_base_url "${baseUrl}" \
+\
       -master_pl_name master.m3u8 \
       -hls_time 6 \
       -hls_list_size 0 \
       -hls_playlist_type vod \
       -hls_segment_filename "${tmpDir}/v%vfileSequence%d.ts" \
       ${tmpDir}/v%vprog_index.m3u8`,
-      { cwd: tmpDir, stdio: 'pipe'}
+      { cwd: tmpDir }
     );
+
+      //     -hls_enc 1 \
+      // -hls_key_info_file "${title}.keyinfo" \
+
+    // execSync(
+    //   `${ffmpeg_static}  -y \
+    //   -i ${originalFilePath} \
+    //   -c:a copy \
+    //   -hls_enc_key key.info \
+    //   -preset fast -sc_threshold 0 \
+    //   -c:v libx264 \
+    //   -filter:v fps=30 -g 60 \
+    //   -map 0 -s:v:0 426x240 -b:v:1 192k \
+    //   -map 0 -s:v:1 640x360 \
+    //   -map 0 -s:v:2 854x480 \
+    //   -map 0 -s:v:3 1280x720 \
+    //   -map 0 -s:v:4 1920x1080 \
+    //   -map 0 -s:v:5 2560x1440 \
+    //   -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" \
+    //   -f hls \
+    //   -hls_key_info_file "${title}.keyinfo" \
+    //   -master_pl_name master.m3u8 \
+    //   -hls_time 6 \
+    //   -hls_list_size 0 \
+    //   -hls_playlist_type vod \
+    //   -hls_segment_filename "${tmpDir}/v%vfileSequence%d.ts" \
+    //   ${tmpDir}/v%vprog_index.m3u8`,
+    //   { cwd: tmpDir, stdio: 'pipe'}
+    // );
 
     console.log(`Segmentation of "${title}" complete`);
     const tmpDirContents = fs.readdirSync(tmpDir);
