23e75b872e66cb8dcafc03e1885f6213716c5a6f
Derek Johnson <derek.johnson@appstem.com>
2020-11-09 22:08:50 -0800

    ...

--- src/index.ts
+++ src/index.ts
@@ -152,13 +152,13 @@ const processVideo = async (sourceBucket: string, gcsFilePath: string, email: st
       -sc_threshold 0 \
       -c:v libx264 \
       -filter:v fps=23.98 -g 60 \
-      -map v:0 -s:v:0 214x120 -b:v:0 128k -maxrate:v:0 141k -bufsize:v:0 212k -profile:v:0 baseline \
+      -map v:0 -s:v:0 214x120 -b:v:0 128k -maxrate:v:0 141k -bufsize:v:0 160k -profile:v:0 baseline \
       -map 0 -s:v:1 640x360 -b:v:1 384k -maxrate:v:1 422k -bufsize:v:1 633k\
       -map 0 -s:v:2 854x480 -b:v:2 512k -maxrate:v:2 563k -bufsize:v:2 845k\
       -map 0 -s:v:3 1280x720 -b:v:3 1024k -maxrate:v:3 1126k -bufsize:v:3 1689k\
       -map 0 -s:v:4 1920x1080 -b:v:4 2056k -maxrate:v:4 2262k -bufsize:v:4 3393k\
       -map 0 -s:v:5 2560x1440 -b:v:5 3212k -maxrate:v:5 3533k -bufsize:v:5 4818k\
-      -map a:0 -c:a:0 aac -b:a:0 32k -ac 1 \
+      -map a:0 -c:a:0 aac -b:a:0 48k -ac 1 \
       -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 v:3,a:3 v:4,a:4 v:5,a:5" \
       -f hls \
       -hls_base_url "${baseUrl}" \
