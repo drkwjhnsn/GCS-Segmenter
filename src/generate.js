2235cb5ef997a8c2d54fea386ed8f718026d17aa
Derek Johnson <derek.johnson@appstem.com>
2020-11-12 11:35:30 -0800

    ...

--- src/generate.js
+++ src/generate.js
@@ -1,4 +1,4 @@
-
+const fetch = require('node-fetch');
 const dotenv =  require('dotenv');
 const path = require('path');
 const {Storage} = require('@google-cloud/storage');
@@ -15,7 +15,28 @@ const main = async () => {
    .bucket(sourceBucket)
    .getFiles({ prefix: "videos/" });
   for (let i = 0; i < videoObjectResponse.length; i++) {
-    console.log(JSON.stringify(videoObjectResponse[i], null, 2))
+    const meta = videoObjectResponse[i].metadata;
+    if (!meta || meta.contentType !== "video/mp4") continue;
+      console.log(JSON.stringify(meta, null, 2));
+
+      const raw = JSON.stringify({
+        name: meta.name.split('/')[1],
+        bucket: "db-method-app.appspot.com",
+      });
+
+      const requestOptions = {
+        method: "POST",
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://us-central1-db-method-app.cloudfunctions.net/manualTrigger",
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
   }
 }
 
